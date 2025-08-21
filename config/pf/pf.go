6d9803e1e87878e63b74dd0f536df352ce154e1e
eking <rbaylon@outlook.com>
2025-08-21 13:00:54 +0800

    keep state explicitly

--- config/pf/pf.go
+++ config/pf/pf.go
@@ -319,10 +319,10 @@ block in quick from <martians>
 
 						subqueue = fmt.Sprintf("%squeue %s%stest parent %s bandwidth %dM min 5M max %dM qlimit 1024\n", subqueue, ident, i.Name, i.Name, planlist[sub.Plan].SpeedTestDown, planlist[sub.Plan].SpeedTestDown)
 
-						subpass = fmt.Sprintf("%spass in quick on $%s inet proto { tcp, udp } from %s to any port { 5060, 8080 } %s set queue (%s%stest, %s%slow) set prio 7 tag \"%stest\"\n",
+						subpass = fmt.Sprintf("%spass in quick on $%s inet proto { tcp, udp } from %s to any port { 5060, 8080 } keep state %s set queue (%s%stest, %s%slow) set prio 7 tag \"%stest\"\n",
 							subpass, i.Name, sub.FramedIp, gateways, ident, i.Name, ident, i.Name, ident)
 
-						subpass = fmt.Sprintf("%spass in on $%s from %s %s set queue (%s%s, %s%slow) %s tag \"%s\"\n",
+						subpass = fmt.Sprintf("%spass in on $%s from %s keep state %s set queue (%s%s, %s%slow) %s tag \"%s\"\n",
 							subpass, i.Name, sub.FramedIp, gateways, ident, i.Name, ident, i.Name, priority, ident)
 					}
 				}

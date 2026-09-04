15cba87f5fa249ef3797cc421da1fda8e28cc76a
eking <eking@live.com.ph>
2026-09-05 06:19:41 +0800

    cleanup

--- configure.sh
+++ configure.sh
@@ -3,13 +3,6 @@
 workdir=$(dirname $0)
 passwd=$1
 cd $workdir
-echo "Setting up system files..."
-mv systemfiles/sysctl.conf /etc/
-mv systemfiles/rc.conf.local /etc/
-cp rc_arkgated /etc/rc.d/arkgated
-chmod -v 755 /etc/rc.d/arkgated
-echo "done"
-
 echo "creating arkgate group"
 groupadd arkgate
 echo "done"
@@ -18,13 +11,6 @@ echo "Creating arkadmin user..."
 adduser -batch arkadmin arkgate $passwd -unencrypted
 echo "done"
 
-echo "Installing arkgated"
-mkdir "/usr/local/arkgate/arkgated"
-go env -w GOBIN=/usr/local/arkgate/arkgated
-echo "enabling arkgated to run on startup"
-echo "arkgated_flags=\"-config ${workdir}/rundir/daemon.config\"" >> /etc/rc.conf.local
-rcctl enable arkgated
-rcctl start arkgated
 cd /dev/
 ./MAKEDEV pppac1
 ./MAKEDEV pppac2
